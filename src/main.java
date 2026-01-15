dcdc413f9b259fd12891bb6d33c367b59bfe2c5a
Is-Lorca <isisloliveira@gmail.com>
2026-01-15 15:40:44 -0300

    First commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        //variables
+        //welcome message
+        //enter balance
+        //play if balance > 0
+        //enter bet amount
+            //verify:
+            //if bet > balance --> don't play
+            //if bet > 0
+            //subtract bet from balance
+        //spin row
+        //print row
+        //get payout (add at balance)
+        //ask play again
+        //goodbye message
+    }
+}
